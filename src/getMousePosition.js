48e539687ab0dbe449563a63657197ce67d12a5d
Zachary Rankin <rankin.zachary@gmail.com>
2024-08-17 05:59:32 +0000

    Remove unused things

--- src/getMousePosition.js
+++
@@ -1,9 +0,0 @@
-export default function getMousePosition(canvas, e) {
-  const r = canvas.getBoundingClientRect();
-  const widthScale = canvas.width / r.width;
-  const heightScale = canvas.height / r.height;
-  const x = (e.clientX - r.left) * widthScale;
-  const y = (e.clientY - r.top) * heightScale;
-
-  return { x, y };
-}
